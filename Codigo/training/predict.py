b8423051402452332178dd31758aa011e641104e
Felipe Mello <felipecbmello@gmail.com>
2024-12-10 08:22:48 -0300

    Implemented OCR for license plates

--- Codigo/training/predict.py
+++ Codigo/training/predict.py
@@ -1,29 +1,72 @@
 import cv2
+import time
 from ultralytics import YOLOv10 as YOLO
+from util import ler_placa, dump_results
 
-# carrega o modelo treinado
+# Carrega o modelo treinado
 model = YOLO('./runs/detect/train1/weights/last.pt')
 
-# inicializa a camera
-video_capture = cv2.VideoCapture(0)
+results = {}
+
+# Inicializa a camera
+video_capture = cv2.VideoCapture(1)
 if not video_capture.isOpened():
-    print("Error: Could not open webcam")
+    print("Erro: Não foi possível abrir a câmera")
     exit()
 
 while True:
     ret, frame = video_capture.read()
     if not ret:
-        print("Error: Can't receive frame from webcam")
+        print("Erro: Não foi possível receber frames da webcam")
         break
 
-    # roda o modelo em cima do frame atual da camera
-    results = model(frame)[0]
+    # Roda o modelo em cima do frame atual da camera
+    detections = model(frame)[0]
 
-    # desenha a caixa
-    for result in results.boxes.data.tolist():
-        x1, y1, x2, y2, conf, cls = result[:6]
+    for detection in detections.boxes.data.tolist():
+        x1, y1, x2, y2, conf, cls = detection[:6]
         label = f'{model.names[cls]} {conf:.2f}'
 
+        # Corta a placa
+        placa_cortada = frame[int(y1):int(y2), int(x1):int(x2), :]
+
+        # Processamento da imagem da placa
+        placa_cinza = cv2.cvtColor(placa_cortada, cv2.COLOR_BGR2GRAY)
+        placa_cinza_hist = cv2.equalizeHist(placa_cinza)
+        _, placa_thresh = cv2.threshold(placa_cinza, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        # Mostrar as placas cortadas
+        # cv2.imshow('original', placa_cortada)
+        # cv2.imshow('cinza', placa_cinza)
+        # cv2.imshow('hist', placa_cinza_hist)
+        # cv2.imshow('thresh', placa_thresh)
+
+        # Lê os caracteres da placa
+        texto_placa, conf_texto_placa = ler_placa(placa_thresh)
+
+        # Inicializa a lista de placas se ela nn existir
+        """
+        ===================================================
+        Implementar estratégia para otimização de leitura de placas de diferentes carros
+        ===================================================
+        """
+        if "placas" not in results:
+            results["placas"] = []
+            start_time = time.perf_counter()
+
+
+        # Concatena as placas na lista
+        if texto_placa is not None:
+            results["placas"].append({
+                "texto": texto_placa,
+                "conf": conf_texto_placa
+            })
+
+        if time.perf_counter() - start_time > 5:
+            dump_results(results)
+            exit()
+
+        # Desenha a caixa
         if conf > 0.5:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 4)
             cv2.putText(frame, label, (int(x1), int(y1) - 10), 
---
+++ Codigo/training/util.py
@@ -0,0 +1,64 @@
+import json
+import string
+import easyocr
+
+reader = easyocr.Reader(['en'], gpu=False)
+
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
+def verificador_formato_placa(text):
+    if len(text) != 7:
+        return False
+
+    # Formato XXX1X11
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in string.ascii_uppercase or text[2] in dict_int_to_char.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in dict_char_to_int.keys()) and \
+       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in dict_char_to_int.keys()):
+        return True
+    else:
+        return False
+
+def formatar_placa(text):
+    placa = ''
+    mapa = {0: dict_int_to_char, 1: dict_int_to_char, 2: dict_int_to_char, 4: dict_int_to_char,
+               3: dict_char_to_int, 5: dict_char_to_int, 6: dict_char_to_int}
+    
+    for j in [0, 1, 2, 3, 4, 5, 6]:
+        if text[j] in mapa[j].keys():
+            placa += mapa[j][text[j]]
+        else:
+            placa += text[j]
+
+    return placa
+
+def ler_placa(placa):
+    detections = reader.readtext(placa)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(" ", "")
+        if verificador_formato_placa(text):
+            return formatar_placa(text), score
+
+    return None, None
+
+def dump_results(results, filename='results.json'):
+   with open(filename, 'w', encoding='utf-8') as f:
+       json.dump(results, f, indent=4, ensure_ascii=False)
